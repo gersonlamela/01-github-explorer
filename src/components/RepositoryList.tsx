ababc03692ba494aae4c56cafcf1e2ad8034e335
Gerson Lamela <gersonlamela7@gmail.com>
2021-12-19 18:35:48 +0000

    TypeScript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,11 +3,17 @@ import { useState, useEffect } from "react";
 
 import '../styles/repositories.scss'
 
+interface Repository{
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 //https://api.github.com/orgs/rocketseat/repos
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
-
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  
   useEffect(() =>{
     fetch('https://api.github.com/orgs/rocketseat/repos')
     .then(response => response.json())
@@ -23,6 +29,7 @@ export function RepositoryList() {
           return <RepositoryItem key={repository.name} repository={repository}/>
         })}
       </ul>
+      
     </section>
   );
 }
